4429b662b983a92e7ad44af3d9994b21a0edb9c1
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-01-23 09:46:16 +0400

    Task 2 fixed fit __name__

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import sys
-length = len(sys.argv) - 1
-if length <= 0:
-    print("0 arguments.")
-else:
-    print("{} arguments:".format(length))
-    for i in range(1, length + 1):
-        print("{}: {}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+    import sys
+    length = len(sys.argv) - 1
+    if length <= 0:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(length))
+        for i in range(1, length + 1):
+            print("{}: {}".format(i, sys.argv[i]))
