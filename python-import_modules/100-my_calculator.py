8d0fc22f4172965a1b4bc6309edc0bed2dc82458
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-01-23 10:21:05 +0400

    Whitespace

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -4,17 +4,19 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
 
     c = len(sys.argv) - 1
+
     if c != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
+
+    operation = sys.argv[2]
     if sys.argv[2] not in ["+", "-", "*", "/"]:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    operation = sys.argv[2]
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    
+
     if operation == "+":
         print("{} + {} = {}".format(a, b, add(a, b)))
     elif operation == "-":
