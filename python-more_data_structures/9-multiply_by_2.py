da274bd4a5117d5ba9e275b71b3fcb578ffa1d96
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-01-29 17:22:57 +0400

    Remove range keyword

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -2,7 +2,7 @@
 def multiply_by_2(a_dictionary):
     new_dict = {}
 
-    for i in range a_dictionary.keys():
+    for i in a_dictionary.keys():
         new_dict[i] = a_dictionary[i] * 2
 
     return new_dict
