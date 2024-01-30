f7a1bae0a82c4a6e09d60fccf7719cb151de62f6
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-01-30 20:50:38 +0400

    also type error

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    flag = True
     try:
         print("{:d}".format(value))
-        flag = True
-    except ValueError:
-        flag = False
-    finally:
-        return flag
+        return True
+    except (TypeError, ValueError):
+        return False
