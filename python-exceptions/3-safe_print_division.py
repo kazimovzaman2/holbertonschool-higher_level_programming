e069d333f51ad180f6ee374c961df260eb1317c4
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-01-30 21:17:07 +0400

    return in each try except

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,8 +2,9 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except (TypeError, ZeroDivisionError):
+        return result
+    except ZeroDivisionError:
         result = None
+        return result
     finally:
         print("Inside result: {}".format(result))
-    return div
