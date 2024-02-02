cff0dc6c456ef719ecac78bfaeea5a4d5f0ad0d4
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-02-02 14:49:32 +0400

    ADd aditional test

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -9,12 +9,9 @@ def add_integer(a, b=98):
     """
     Basic 2 integer addition
     """
-    if type(a) not in [int, float]:
+    if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
+    if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
 
-    a = int(a)
-    b = int(b)
-
-    return a + b
+    return int(a) + int(b)
