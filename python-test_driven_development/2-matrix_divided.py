d3f65c6b19966f5deb526ce10bab23570b54ac0f
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-02-03 09:00:04 +0400

    Task 2

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""
+Divide all elements of a matrix by a given number.
+"""
+
+
+def matrix_divided(matrix, div):
+    """ """
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    if not all(isinstance(num, (int, float)) for row in matrix for num in row):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    if len(set(len(row) for row in matrix)) > 1:
+        raise TypeError("Each row of the matrix must have the same size")
+
+    result = []
+    for row in matrix:
+        new_row = []
+        for elem in row:
+            new_row.append(round(elem / div, 2))
+        result.append(new_row)
+
+    return result
