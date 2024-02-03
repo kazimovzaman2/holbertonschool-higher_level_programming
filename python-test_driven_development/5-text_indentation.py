62450b8c4744f3d52a8ba9fbc3772f3003434283
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-02-03 09:52:36 +0400

    Final new line

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -14,12 +14,16 @@ def text_indentation(text):
         raise TypeError("text must be a string")
 
     clean_text = text.strip()
-
     result = ""
+    new_line = False
 
     for c in clean_text:
         result += c
         if c in ".?:":
             result += "\n\n"
+            new_line = True
+        elif c != " " and new_line:
+            result += "\n"
+            new_line = False
 
     print(result)
