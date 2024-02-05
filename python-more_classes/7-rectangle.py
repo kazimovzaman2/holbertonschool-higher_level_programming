f161e95a2a039eb602823653c1f217051d360484
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-02-05 11:58:56 +0400

    Fix syntax error

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -58,7 +58,7 @@ class Rectangle:
         rect = ""
         for i in range(self.__height):
             for j in range(self.__width):
-                rect += RecRectangle.print_symbol
+                rect += Rectangle.print_symbol
             if i != self.__height - 1:
                 rect += "\n"
         return rect
