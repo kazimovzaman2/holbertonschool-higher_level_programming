87d97a72c4ce42d2e2acb0bd43b50873524039dd
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-02-06 09:59:01 +0400

    Change documentaiton

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 
-"""
-Module doc
-"""
+"""Module doc"""
 
 
 def inherits_from(obj, a_class):
-    """Same"""
+    """
+    Same
+    """
     if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     return False
