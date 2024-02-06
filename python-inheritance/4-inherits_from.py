b1880f79859181ed036663187efe3c62ee0c838e
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-02-06 10:07:08 +0400

    Idk

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -8,6 +8,6 @@ def inherits_from(obj, a_class):
     """
     Same
     """
-    if issubclass(type(obj), a_class) and type(obj) not is a_class:
+    if issubclass(type(obj), a_class) and type(obj) != a_class:  # noqa E721
         return True
     return False
