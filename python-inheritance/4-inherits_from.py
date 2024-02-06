0475fcb5f84bff9290c436393bac791bfcfad790
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-02-06 09:57:47 +0400

    Add additional check

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -8,4 +8,6 @@ Module doc
 
 def inherits_from(obj, a_class):
     """Same"""
-    return issubclass(type(obj), a_class)
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
