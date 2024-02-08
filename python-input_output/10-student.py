c5962a8fbf623574ab9ea92f12785d0135d9eef3
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-02-08 11:57:28 +0400

    Task 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+"""Documented Module"""
+
+
+class Student:
+    """Class document"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if not attrs:
+            return self.__dict__
+        else:
+            result = {}
+            for attr in attrs:
+                if attr in self.__dict__:
+                    result[attr] = self.__dict__[attr]
+            return result
