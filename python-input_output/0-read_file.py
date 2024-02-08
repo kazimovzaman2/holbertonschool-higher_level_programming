91e1c70c02cefd76506857372f73dd745da34898
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-02-08 10:32:58 +0400

    Documentation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 
+"""Documented Module"""
+
+
 def read_file(filename=""):
+    """Documented function"""
     with open(filename, encoding="utf-8") as f:
         print(f.read())
