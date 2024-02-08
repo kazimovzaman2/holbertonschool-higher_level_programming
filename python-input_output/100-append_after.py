66f62599098beb7d6a010737e6dd91aad8e82878
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-02-08 13:46:56 +0400

    Mode is need to be read

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -7,7 +7,7 @@
 def append_after(filename="", search_string="", new_string=""):
     """Doc for function"""
     text = ""
-    with open(filename, "ir", encoding="utf-8") as rf:
+    with open(filename, "r", encoding="utf-8") as rf:
         for line in rf:
             text += line
             if search_string in line:
