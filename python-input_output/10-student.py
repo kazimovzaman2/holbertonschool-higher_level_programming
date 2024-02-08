19e95f78c19882133ca282e61799d1252e4f8401
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-02-08 11:58:48 +0400

    If statement must be check is None

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -12,7 +12,7 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        if not attrs:
+        if attrs is None:
             return self.__dict__
         else:
             result = {}
