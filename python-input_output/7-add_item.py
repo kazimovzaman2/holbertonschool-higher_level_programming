154e5bdb5276b6cf232d39211408b167cf3f38c2
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-02-08 11:12:05 +0400

    Import os

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,7 +4,7 @@
 """Documented Module"""
 
 
-import json, sys
+import json, sys, os
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
