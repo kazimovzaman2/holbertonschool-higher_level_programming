0f9b9ec460d737bf18d65c7ed0ffb872374ed3fc
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-02-08 10:36:06 +0400

    With for loop

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -7,4 +7,5 @@
 def read_file(filename=""):
     """Documented function"""
     with open(filename, encoding="utf-8") as f:
-        print(f.read())
+        for line in f:
+            print(line, end="")
