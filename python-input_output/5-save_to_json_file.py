e49d00fe73b22c9d503ae91f207023e2dbe4be5e
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-02-08 11:00:25 +0400

    Json dump is more usefull

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -9,7 +9,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """Documented function"""
-    formated_text = json.dumps(my_obj)
-
-    with open(filename, "w", encoding="utf-8") as f:
-        return f.write(formated_text)
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
