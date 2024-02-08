ee58bbacb86b1224d9b07cf2d4128377e29e62db
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-02-08 11:01:58 +0400

    Task 6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+"""Documented Module"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """Documented function"""
+    with open(filename, "r") as f:
+        json.load(f)
