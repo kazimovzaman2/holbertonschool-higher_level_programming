75c9ac5b14a9b7482fffa7bff6e28f30398f0aeb
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-02-08 14:40:42 +0400

    Check

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -6,4 +6,6 @@
 
 def add_attribute(obj, attr, value):
     """Documented function"""
+    if "__dict__" not in dir(obj):
+        raise TypeError("can't add new attribute")
     obj.__dict__[attr] = value
