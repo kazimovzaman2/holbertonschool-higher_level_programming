54872adcbbafbb8b471d0ee52ecd02134dc648fd
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-02-08 13:46:14 +0400

    Not search string but also new string

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -7,11 +7,11 @@
 def append_after(filename="", search_string="", new_string=""):
     """Doc for function"""
     text = ""
-    with open(filename, "r", encoding="utf-8") as rf:
+    with open(filename, "ir", encoding="utf-8") as rf:
         for line in rf:
             text += line
             if search_string in line:
-                text += search_string
+                text += new_string
 
     with open(filename, "w", encoding="utf-8") as wf:
         wf.write(text)
