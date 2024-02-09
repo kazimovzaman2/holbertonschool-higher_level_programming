a5aaba3fab890ac7381e550fbca3b4608108658a
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-02-09 08:45:45 +0400

    TAsk update 101

--- python-more_data_structures/101-square_matrix_map.py
+++ python-more_data_structures/101-square_matrix_map.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    def square_row(row):
-        return list(map(square_element, row))
-
-    def square_element(x):
-        return x ** 2
-
-    return list(map(square_row, matrix))
+    return list(map(lambda row: list(map(lambda x: x**2, row)), matrix))
