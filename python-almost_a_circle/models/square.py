cbc3a14e8e6b344647474a5486a5b7299ac9b015
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-02-12 12:55:14 +0400

    Change __x to x

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -19,4 +19,4 @@ class Square(Rectangle):
         self.height = value
 
     def __str__(self):
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.__x, self.__y, self.size)
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.size)
