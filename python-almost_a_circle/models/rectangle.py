12c180541184f501b9ff1468692ded1f60196861
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-02-12 12:17:12 +0400

    Change import statemt

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Rectangle class that inherits from Base class."""
-# from models.base import Base
-Base = __import__("base").Base
+from models.base import Base
 
 
 class Rectangle(Base):
