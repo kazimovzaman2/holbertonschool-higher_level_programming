e2329c193b1896732c3a82bfee76b08a4a696b30
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-02-12 11:05:05 +0400

    Add 'an' article before integer

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -30,7 +30,7 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         if type(value) != int:
-            raise TypeError("width must be integer")
+            raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
@@ -43,7 +43,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         if type(value) != int:
-            raise TypeError("height must be integer")
+            raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
@@ -56,7 +56,7 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         if type(value) != int:
-            raise TypeError("x must be integer")
+            raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
@@ -69,7 +69,7 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         if type(value) != int:
-            raise TypeError("y must be integer")
+            raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
