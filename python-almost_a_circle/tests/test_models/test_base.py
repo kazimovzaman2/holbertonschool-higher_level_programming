743e72d8be63a86196ef216c1535827ffca42dd1
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-02-12 15:12:35 +0400

    Id

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -5,14 +5,29 @@ from models.base import Base
 
 
 class TestBase(unittest.TestCase):
-    def test_init(self):
-        base = Base()
-        self.assertIsInstance(base, Base)
-        self.assertEqual(base.id, 1)
+    def test_id_as_positive(self):
+        base_instance = Base(115)
+        self.assertEqual(base_instance.id, 115)
+        base_instance = Base(67)
+        self.assertEqual(base_instance.id, 67)
 
-    def test_id_exists(self):
-        base = Base()
-        self.assertTrue(hasattr(base, "id"))
+    def test_id_as_negative(self):
+        base_instance = Base(-91)
+        self.assertEqual(base_instance.id, -91)
+        base_instance = Base(-4)
+        self.assertEqual(base_instance.id, -4)
+
+    def test_id_as_none(self):
+        base_instance = Base()
+        self.assertEqual(base_instance.id, 1)
+        base_instance = Base(None)
+        self.assertEqual(base_instance.id, 2)
+
+    def test_string_id(self):
+        base_instance = Base("Monty Python")
+        self.assertEqual(base_instance.id, "Monty Python")
+        base_instance = Base("Python is cool")
+        self.assertEqual(base_instance.id, "Python is cool")
 
 
 if __name__ == "__main__":
