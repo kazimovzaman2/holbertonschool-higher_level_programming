d773c8278af377756538392ca7a8578c79531ce4
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-02-12 10:23:45 +0400

    Rectangle class

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+
+"""Rectangle class in model package"""
+
+
+Base = __import__('base').Base
+
+
+class Rectangle(Base):
+    """Rectangle class that inherits from Base class"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id=id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
