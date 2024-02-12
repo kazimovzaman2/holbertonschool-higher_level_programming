726342e2717ed3fb755f8d7243abf568e6b3d373
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-02-12 15:33:41 +0400

    Add more test cases.

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """Test for rectangle model"""
 import unittest
-from models.base import Base
 from models.rectangle import Rectangle
 
 
 class TestBase(unittest.TestCase):
     def test_rectangle(self):
-        r1 = Rectangle(1, 2, id=3)
-        self.assertIsNotNone(r1)
+        r0 = Rectangle(1, 2, id=1)
+        self.assertEqual(r0.id, 1)
+        r1 = Rectangle(2, 3, id=12)
+        self.assertEqual(r1.id, 12)
 
 
 if __name__ == "__main__":
