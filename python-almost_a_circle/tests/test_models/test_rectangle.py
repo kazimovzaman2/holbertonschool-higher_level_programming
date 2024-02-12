0d55bc6f636bff5e2a7ccd8ebb9a406e01cdd6dc
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-02-12 15:49:56 +0400

    Add more test cases.

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -9,12 +9,16 @@ class TestBase(unittest.TestCase):
         r0 = Rectangle(1, 2)
         self.assertEqual(r0.width, 1)
         self.assertEqual(r0.height, 2)
+        r1 = Rectangle(1, 2, 3, 4, 5)
+        self.assertEqual(r1.width, 1)
+        self.assertEqual(r1.height, 2)
+        self.assertEqual(r1.x, 3)
+        self.assertEqual(r1.y, 4)
+        self.assertEqual(r1.id, 5)
 
-    def test_no_args(self):
+    def test_type_errors(self):
         with self.assertRaises(TypeError):
             Rectangle()
-
-    def test_type_args(self):
         with self.assertRaises(TypeError):
             Rectangle(1)
         with self.assertRaises(TypeError):
@@ -25,6 +29,20 @@ class TestBase(unittest.TestCase):
             Rectangle(1, 2, "3")
         with self.assertRaises(TypeError):
             Rectangle(1, 2, 3, "4")
+
+    def test_value_errors(self):
+        with self.assertRaises(ValueError):
+            Rectangle(-1, 2)
+        with self.assertRaises(ValueError):
+            Rectangle(1, -2)
+        with self.assertRaises(ValueError):
+            Rectangle(0, 2)
+        with self.assertRaises(ValueError):
+            Rectangle(1, 0)
+        with self.assertRaises(ValueError):
+            Rectangle(1, 2, -3)
+        with self.assertRaises(ValueError):
+            Rectangle(1, 2, 3, -4)
 
 
 if __name__ == "__main__":
