8dfce942489f03aa583e3993a3f3039fd35ba166
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-02-12 15:05:52 +0400

    Init

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 """Test for base model"""
-
-import unittest
-from models.base import Base
-from models.rectangle import Rectangle
-from models.square import Square
 import unittest
 from models.base import Base
 
@@ -14,26 +9,6 @@ class TestBase(unittest.TestCase):
         base = Base()
         self.assertIsInstance(base, Base)
         self.assertEqual(base.id, 1)
-
-        base2 = Base()
-        self.assertIsInstance(base2, Base)
-        self.assertEqual(base2.id, 2)
-
-        base3 = Base(10)
-        self.assertIsInstance(base3, Base)
-        self.assertEqual(base3.id, 10)
-
-    def test_auto_assign_id(self):
-        base1 = Base()
-        base2 = Base()
-        base3 = Base()
-        self.assertEqual(base1.id, 3)
-        self.assertEqual(base2.id, 4)
-        self.assertEqual(base3.id, 5)
-
-    def test_id_exists(self):
-        base = Base(100)
-        self.assertEqual(base.id, 100)
 
 
 if __name__ == "__main__":
