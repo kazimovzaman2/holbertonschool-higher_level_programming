3f11f3f287afa208f5e701bcf2159d9c1cf49abc
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-02-12 11:00:08 +0400

    Change type check.

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -30,7 +30,7 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("width must be integer")
         if value <= 0:
             raise ValueError("width must be > 0")
@@ -43,7 +43,7 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("height must be integer")
         if value <= 0:
             raise ValueError("height must be > 0")
@@ -56,7 +56,7 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("x must be integer")
         if value < 0:
             raise ValueError("x must be >= 0")
@@ -69,7 +69,7 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("y must be integer")
         if value < 0:
             raise ValueError("y must be >= 0")
