887c59aba1646f4d66e4dc1f36d2bb15bcfb27ea
Zaman Kazimov <kazimovzaman2@gmail.com>
2024-03-14 18:04:13 +0400

    Task 9

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,13 @@
+-- Full creation
+CREATE TABLE IF NOT EXISTS `second_table` (
+        id int,
+        name varchar(256)
+	score int
+);
+INSERT INTO `second_table` (id, name, score)
+VALUES
+	(1, "John", 10),
+	(2, "Alex", 3),
+	(3, "Bob", 14),
+	(4, "George", 8);
+
