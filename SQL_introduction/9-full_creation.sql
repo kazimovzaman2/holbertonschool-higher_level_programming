cbf4bf51e2e95965a33c39ba036287d42ca9dc50
Zaman Kazimov <kazimovzaman2@gmail.com>
2024-03-14 18:08:43 +0400

    Add ','

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- Full creation
 CREATE TABLE IF NOT EXISTS `second_table` (
         `id` INT,
-        `name` VARCHAR(256)
+        `name` VARCHAR(256),
 	`score` INT
 );
 INSERT INTO `second_table` (id, name, score)
