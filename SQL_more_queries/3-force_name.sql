39c9ac9ef55b35accbd517b93d11f1ae4196074b
Zaman Kazimov <kazimovzaman2@gmail.com>
2024-03-20 18:13:45 +0400

    Task 4

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,5 +1,5 @@
 -- Always a name
 CREATE TABLE IF NOT EXISTS `force_name` (
   id INT,
-  name VARCHAR(100) NOT NULL
+  name VARCHAR(256) NOT NULL
 );
---
+++ SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,5 @@
+-- ID can't be null
+CREATE TABLE IF NOT EXISTS `id_not_null` (
+  id INT NOT NULL
+  name VARCHAR(256) NOT NULL
+);
