aafc58e10b83e3ed436d6e198dbb3f06cf58dcd5
Zaman Kazimov <kazimovzaman2@gmail.com>
2024-03-20 18:53:25 +0400

    Task 10 - needed comma

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
 -- Genre ID by show
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_show_genres
-LEFT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
