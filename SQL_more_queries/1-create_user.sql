7d3a09a2d66e98afcbc276c544442d88b0b7cb4d
Zaman Kazimov <kazimovzaman2@gmail.com>
2024-03-20 12:17:47 +0400

    Reset permission table

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- Root user
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY user_0d_1_pwd;
-GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
+FLUSH PRIVILEGES;
 
