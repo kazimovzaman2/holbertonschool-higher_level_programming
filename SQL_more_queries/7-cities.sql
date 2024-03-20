5ab0c1bd91ea413fc42358178375d5033555426f
Zaman Kazimov <kazimovzaman2@gmail.com>
2024-03-20 18:23:31 +0400

    Task 7

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- Cities table
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+USE `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `cities` (
+  `id` INT AUTO_INCREMENT NOT NULL UNIQUE PRIMARY KEY,
+  `name` VARCHAR(256) NOT NULL,
+  `state_id` INT NOT NULL,
+  FOREIGN KEY (`state_id`) REFERENCES `states` (`id`)
+);
