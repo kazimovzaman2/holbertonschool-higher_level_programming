113b593ae094687e2956a62abfd6bd52dfa864f4
Zaman Kazimov <kazimovzaman2@gmail.com>
2024-03-20 18:20:14 +0400

    Task 6

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- States table
+CREATE DATABASE `hbtn_0d_usa`;
+USE `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `states` (
+  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `name` VARCHAR(256) NOT NULL
+);
