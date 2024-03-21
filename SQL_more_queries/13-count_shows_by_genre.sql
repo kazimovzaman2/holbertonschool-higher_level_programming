2ddb79e1b0ef83a27b420b7f4d7bb366c449a505
Zaman Kazimov <kazimovzaman2@gmail.com>
2024-03-21 15:21:12 +0400

    Task 13

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,6 @@
 -- Number of shows by genre
-SELECT tv_genres.name as genre, COUNT(*) as number_of_shows
-FROM tv_genres
-INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_genres.name
+SELECT tv_genres.name AS genre, COUNT(show_id) AS number_of_shows
+FROM tv_genres JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre
 ORDER BY number_of_shows DESC;
 
