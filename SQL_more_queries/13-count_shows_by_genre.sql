58940c87af6de85ded690c48c0ea0d9a6c1584de
Zaman Kazimov <kazimovzaman2@gmail.com>
2024-03-21 15:16:43 +0400

    This is not id

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,7 @@
 -- Number of shows by genre
 SELECT tv_genres.name as genre, COUNT(*) as number_of_shows
 FROM tv_genres
-INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.id
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
+
