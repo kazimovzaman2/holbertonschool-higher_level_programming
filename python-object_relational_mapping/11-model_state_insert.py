bf32f6c364cfacc60ff747ecda444cd1b1a7bd62
Zaman Kazimov <kazimovzaman2@gmail.com>
2024-03-22 17:43:23 +0400

    Task 11

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This script fetches all the states from the database and prints them.
+"""
+
+import sys
+from model_state import Base, State
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    """
+    Main entry point of the script.
+    """
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True,
+    )
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    louisiana = State(name="Louisiana")
+
+    session.add_all([louisiana])
+    session.commit()
+
+    print(louisiana.id)
