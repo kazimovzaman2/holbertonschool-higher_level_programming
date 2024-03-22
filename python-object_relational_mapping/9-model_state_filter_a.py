6bf9ad9f6000ed7143db70613850c92847a41ba2
Zaman Kazimov <kazimovzaman2@gmail.com>
2024-03-22 17:30:41 +0400

    Task 9

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This script fetches all the states from the database and prints them.
+"""
+
+import sys
+from model_state import Base, State
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    """
+    Main entry point of the script.
+    """
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True,
+    )
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    for state in (
+        session.query(State).order_by(State.id).filter(State.name.contains("a")).all()
+    ):
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
