eaa93a42c71cbe88b38818eb65dcd188a324f3b1
Zaman Kazimov <kazimovzaman2@gmail.com>
2024-03-22 15:38:03 +0400

    Task 1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+Filter states by user input
+"""
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+    )
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+
+    cur.close()
+    conn.close()
