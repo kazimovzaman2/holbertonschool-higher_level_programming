3e3e32ee2d1c9e222af043027f35d1291ff1ad68
Zaman Kazimov <kazimovzaman2@gmail.com>
2024-03-22 17:26:10 +0400

    Task 8

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This script fetches all the states from the database and prints them.
+"""
+
+import sys
+from model_state import Base, State
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    """
+    Main entry point of the script.
+    """
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True,
+    )
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    state = session.query(State).order_by(State.id).first()
+    print("{}: {}".format(state.id, state.name))
+
+    session.close()
