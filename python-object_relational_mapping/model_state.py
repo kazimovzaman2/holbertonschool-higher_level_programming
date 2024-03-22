2e3251b2f267e784fbb20256a1c3084b8331c056
Zaman Kazimov <kazimovzaman2@gmail.com>
2024-03-22 17:14:34 +0400

    Task 6: add documentation for class

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -12,7 +12,17 @@ Base = declarative_base()
 
 
 class State(Base):
+    """
+    Represents a state in the database.
+    """
+
     __tablename__ = "states"
 
-    id = Column(Integer, primary_key=True)
+    id = Column(
+        Integer,
+        primary_key=True,
+        unique=True,
+        nullable=False,
+        autoincrement=True,
+    )
     name = Column(String(128), nullable=False)
