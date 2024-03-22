f205cf0ada26afc40ec1ef984594696ba2a8d739
Zaman Kazimov <kazimovzaman2@gmail.com>
2024-03-22 16:59:29 +0400

    Dont need to print states.name

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
 
     cur.execute(
         """
-                SELECT cities.id, cities.name, states.name FROM cities
+                SELECT cities.id, cities.name FROM cities
                 JOIN states ON cities.state_id = states.id
                 WHERE states.name LIKE BINARY %s
                 ORDER BY id
