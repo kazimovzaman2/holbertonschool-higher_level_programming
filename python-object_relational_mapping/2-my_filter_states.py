70f82d5c6a6a851a3387339a69437c1a3d828105
Zaman Kazimov <kazimovzaman2@gmail.com>
2024-03-22 15:46:46 +0400

    Not directly equal, but likelyequal

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
 
     cur.execute(
         """
-                SELECT * FROM states WHERE name = "{}" ORDER BY id
+                SELECT * FROM states WHERE name LIKE BINARY "{}" ORDER BY id
                 """.format(
             sys.argv[4]
         )
