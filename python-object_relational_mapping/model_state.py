b6aca504163b443c42affb3dcae06790d7d35403
Zaman Kazimov <kazimovzaman2@gmail.com>
2024-03-22 17:12:06 +0400

    Task 6

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+Filter states by user input
+"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = "states"
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        unique=True,
+        nullable=False,
+        autoincrement=True,
+    )
+    name = Column(String(128), nullable=False)
