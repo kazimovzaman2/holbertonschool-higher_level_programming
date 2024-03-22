15be5acf9fbd70c8cfc89c4d85269a979564a0d5
Zaman Kazimov <kazimovzaman2@gmail.com>
2024-03-22 16:56:48 +0400

    Spelling error in city table

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
 
     cur.execute(
         """
-                SELECT cities.id, city.name, states.name FROM cities
+                SELECT cities.id, cities.name, states.name FROM cities
                 JOIN states ON cities.state_id = states.id
                 ORDER BY id
                 """
