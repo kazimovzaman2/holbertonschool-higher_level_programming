555c63926bad1c7ba9c20762ba1387d851d2bb96
Zaman Kazimov <kazimovzaman2@gmail.com>
2024-03-22 16:58:41 +0400

    Task 5

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""
+Filter states by user input
+"""
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+    )
+    cur = conn.cursor()
+
+    cur.execute(
+        """
+                SELECT cities.id, cities.name, states.name FROM cities
+                JOIN states ON cities.state_id = states.id
+                WHERE states.name LIKE BINARY %s
+                ORDER BY id
+                """,
+        (sys.argv[4],),
+    )
+
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+
+    cur.close()
+    conn.close()
