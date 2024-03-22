66e114536511d2e11b29b66c743ae95e3153af31
Zaman Kazimov <kazimovzaman2@gmail.com>
2024-03-22 17:39:45 +0400

    Pycodestyle fixed

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     session = Session(engine)
 
     state = session.query(State).filter(State.name == sys.argv[4]).first()
-    
+
     if state is None:
         print("Not found")
     else:
