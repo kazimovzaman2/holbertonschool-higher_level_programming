80e9f59483c8cb3608ca526f89547052b4368e89
Zaman Kazimov <kazimovzaman2@gmail.com>
2024-03-22 17:27:41 +0400

    Print if steate is none

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -25,6 +25,9 @@ if __name__ == "__main__":
     session = Session(engine)
 
     state = session.query(State).order_by(State.id).first()
-    print("{}: {}".format(state.id, state.name))
+    if state is None:
+        print("Nothing")
+    else:
+        print("{}: {}".format(state.id, state.name))
 
     session.close()
