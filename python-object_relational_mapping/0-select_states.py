8d5c24060b248b70183b97e828d9c5e365a1fbda
Zaman Kazimov <kazimovzaman2@gmail.com>
2024-03-22 15:35:01 +0400

    Task 0

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+    )
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM states")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+
+    cur.close()
+    conn.close()
