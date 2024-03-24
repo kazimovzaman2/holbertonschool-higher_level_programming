57222e829e861c5564df8c83b4bb7b9daa420200
Zaman Kazimov <kazimovzaman2@gmail.com>
2024-03-24 10:57:16 +0400

    Task 102

---
+++ python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This script fetches all the states from the database and prints them.
+"""
+
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    """
+    Main entry point of the script.
+    """
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True,
+    )
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    for city in session.query(City).order_by(City.id):
+        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
+
+    session.close()
